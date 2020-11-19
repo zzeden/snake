50b7319931e670d175a896806b819843da9f8fb8
Eden Zabarsky <eden.zabarsky@gmail.com>
2020-11-19 19:02:03 +0200

    add row

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import pygame
 SIZE_BLOCK = 20
 FRAME_COLOR = (0, 255, 204)
 WHITE = (255, 255, 255)
+BLUE = (204, 255, 255)
 COUNT_BLOCKS = 20
+MARGIN = 1
 
 size = [400, 600]
 
@@ -18,7 +20,12 @@ while True:
             pygame.quit()
 
     screen.fill(FRAME_COLOR)
-    for calonka in range(COUNT_BLOCKS):
-        pygame.draw.rect(screen, WHITE, [10 + SIZE_BLOCK * calonka, 20, SIZE_BLOCK, SIZE_BLOCK])
+    for row in range(COUNT_BLOCKS):
+        for calonka in range(COUNT_BLOCKS):
+            if calonka % 2 == 0:
+                color = BLUE
+            else:
+                color = WHITE
+            pygame.draw.rect(screen, color, [10 + SIZE_BLOCK * calonka + calonka, 20 + SIZE_BLOCK * row + row, SIZE_BLOCK, SIZE_BLOCK])
 
     pygame.display.flip()
