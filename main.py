5bcda10400409dea33ac2c9c7d808e5bf60309d0
Eden Zabarsky <eden.zabarsky@gmail.com>
2020-11-30 18:36:47 +0200

    snake block, draw snake

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import pygame
+import time
 
 SIZE_BLOCK = 20
 FRAME_COLOR = (0, 255, 204)
 WHITE = (255, 255, 255)
 BLUE = (204, 255, 255)
 HEADER_COLOR = (0, 204, 153)
+SNAKE_COLOR = (0, 102, 0)
 COUNT_BLOCKS = 20
 HEADER_MARGIN = 70
 MARGIN = 1
 
+snake_block = [(1, 1), (1, 2), (1, 3), (2, 3)]
 size = [(SIZE_BLOCK + MARGIN) * COUNT_BLOCKS + 20, (SIZE_BLOCK + MARGIN) * COUNT_BLOCKS + HEADER_MARGIN + 10]
 
 screen = pygame.display.set_mode(size)
@@ -23,11 +26,12 @@ def draw_block(colori, rowi, columni):
 
 while True:
     for event in pygame.event.get():
-        # print(event)
+
         if event.type == pygame.QUIT:
             print("exit")
             pygame.quit()
-
+        if event.type == pygame.key.key_code("down"):
+            print("down")
     screen.fill(FRAME_COLOR)
     pygame.draw.rect(screen, HEADER_COLOR, [0, 0, size[0], HEADER_MARGIN])
 
@@ -44,5 +48,9 @@ while True:
                 else:
                     color = BLUE
             draw_block(color, row, calonka)
+
+    time.sleep(1)
+    for s in snake_block:
+        draw_block(SNAKE_COLOR, s[0] ,s[1])
 
     pygame.display.flip()
